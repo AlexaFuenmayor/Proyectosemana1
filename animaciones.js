e993d73a08444c2d07e83457e5284e94e01f51f3
alexfuenmayor <estherf.m18@gmail.com>
2021-09-21 20:55:25 -0500

    primer commit

---
+++ animaciones.js
@@ -0,0 +1,41 @@
+alert("¡Hola! Bienvenidos a mi Página Hillsong /o/");
+
+console.log("¡Hola! Bienvenidos a mi Página Hillsong /o/");
+
+prompt("Ingresa Nombre de tu canción de Hillsong Preferida");
+
+confirm("¿Quieres salir de esta página?")
+
+const Cancion1 = () => {
+prompt ("Canción Preferida");
+alert(Cancion);
+}
+
+const Cancion2 = () => {
+prompt ("Canción Preferida");
+alert(Cancion);
+}
+
+const Cancion3 = () => {
+prompt ("Canción Preferida");
+alert(Cancion);
+}
+
+const Cancion4 = () => {
+prompt ("Canción Preferida");
+alert(Cancion);
+}
+
+const Cancion5= () => {
+prompt ("Canción Preferida");
+alert(Cancion);
+ }
+
+ const Cancion6 = () => {
+prompt ("Canción Preferida");
+alert(Cancion);
+ }
+
+ 
+
+
