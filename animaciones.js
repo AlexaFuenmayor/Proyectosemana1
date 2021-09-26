877c8c12fc725e169b9d66c17597e5570e932b09
alexfuenmayor <estherf.m18@gmail.com>
2021-09-25 19:11:32 -0500

    desplegando pagina web

--- animaciones.js
+++ animaciones.js
@@ -2,40 +2,34 @@ alert("¡Hola! Bienvenidos a mi Página Hillsong /o/");
 
 console.log("¡Hola! Bienvenidos a mi Página Hillsong /o/");
 
-prompt("Ingresa Nombre de tu canción de Hillsong Preferida");
-
-confirm("¿Quieres salir de esta página?")
-
 const Cancion1 = () => {
-prompt ("Canción Preferida");
-alert(Cancion);
-}
+  prompt("Ingresa Nombre de tu canción Preferida");
+  alert("El álbum incluye una selección de 14 canciones de Hillsong traducidas y grabadas en español");
+};
 
 const Cancion2 = () => {
-prompt ("Canción Preferida");
-alert(Cancion);
-}
+  prompt("Ingresa Nombre de tu canción Preferida");
+  alert(
+    "Look to You es el sexto álbum en vivo grabado por Hillsong United y la séptima producción de la banda. Fue grabado durante el Encounterfest el 9 de octubre de 2004 y lanzado el 28 de febrero de 2005. "
+  );
+};
 
 const Cancion3 = () => {
-prompt ("Canción Preferida");
-alert(Cancion);
-}
+  prompt("Ingresa Nombre de tu canción Preferida");
+  alert("El set list completo del disco está compuesto por: Por Tí (Go), Nada Hay Que Esconder (No Reason To Hide), Amor Como Fuego (Love Like Fire), Lo Grande Que Eres Dios (The Greatness Of Our God), Reinas Por la Eternidad (Forever Reigns), Conmigo Estás (You Hold Me Now), Tú (You), Nunca Fallará (Rise), Dios Sobre Todo (The Lost Are Found), Señor Te Exalto (I Will Exalt You), En Mi Lugar (Beautiful Exchange) y Dios es Poderoso (God Is Able).");
+};
 
 const Cancion4 = () => {
-prompt ("Canción Preferida");
-alert(Cancion);
-}
+  prompt("Ingresa Nombre de tu canción Preferida");
+  alert("El propósito de «El eco de su voz» es inspirar a quien lo escucha a adorar a Dios, al revelar canción tras canción la hermosura de su amor, su nombre, su gracia, su grandeza, ");
+};
 
-const Cancion5= () => {
-prompt ("Canción Preferida");
-alert(Cancion);
- }
+const Cancion5 = () => {
+  prompt("Ingresa Nombre de tu canción Preferida");
+  alert("Mighty to Save es el decimoquinto álbum de música cristiana contemporánea de la serie alabanza y adoración interpretado por la iglesia Hillsong.");
+};
 
- const Cancion6 = () => {
-prompt ("Canción Preferida");
-alert(Cancion);
- }
-
- 
-
-
+const Cancion6 = () => {
+  prompt("Ingresa Nombre de tu canción Preferida");
+  alert("El nombre del álbum, «En esto creo», se basa en el tema original en inglés, «This I believe (The creed)»,  una recopilación de las canciones más destacadas de Hillsong Worship en los últimos años, que, adaptadas a nuestro idioma, son interpretadas por las voces originales de los proyectos de Hillsong en inglés.");
+};
